a1ed0de3a621f77c06db46688297cfe9109c8f0e
ubuntuServer64 <sonic@localhost.(none)>
2016-10-05 22:43:35 +0800

    update

--- cpp/1_BOOK_DATA_STRUCTURE_C_YANWEIMIN_WUWEIMIN/include/2.1_func.h
+++ cpp/1_BOOK_DATA_STRUCTURE_C_YANWEIMIN_WUWEIMIN/include/2.1_func.h
@@ -7,6 +7,7 @@ bool visit(int* a);
 
 int f_test_linear_list() ;
 int f_test_Stack();
+int f_test_LinkList();
 bool compare(int a,int b);
 bool visit(int *a);
 #endif //M_2_1_func_H
--- cpp/1_BOOK_DATA_STRUCTURE_C_YANWEIMIN_WUWEIMIN/include/2.3_link_list.h
+++ cpp/1_BOOK_DATA_STRUCTURE_C_YANWEIMIN_WUWEIMIN/include/2.3_link_list.h
@@ -1,9 +1,65 @@
 #ifndef M_2_3_LINK_LIST_H
 #define M_2_3_LINK_LIST_H
-struct LNode {
-	int data;
-	LNode * next;
+class Node{
+public:
+	Node(){
+	}
+	Node(int d ,Node* p)
+	{
+		m_data=d;
+		m_next=p;
+	}
+	~Node()
+	{
+	}
+	int GetData(){return m_data;}
+	Node* GetNext(){
+		return m_next;
+	}
+private:
+	int m_data;
+	Node* m_next;
 };
 
+class MyLinkList {
+	public:
+		MyLinkList(); // a blank list, name is NA
+		MyLinkList(string name);
+		MyLinkList(int[],int);
+		MyLinkList(int[],int,string);
+		MyLinkList(MyLinkList& other);//copying construct func
+		MyLinkList & operator=(MyLinkList& other);//copying construct func
+		~MyLinkList();
+
+
+		void SetName(string);
+		void PrintItem(int);
+		void PrintList(int printLength=-1,bool b=false); // default value setting is only allowed in declaration(h files)
+		void ClearList();
+		bool ListEmpty();
+		int ListLength();
+		int GetElem(int ind);
+		void SetElem(int ind,int value);
+		int LocateElem(int e,bool (*compare)(int,int)); // notice: the format of function-pointer
+		int PriorElem(int cur_e);
+		int NextElem (int cur_e);
+		void ListInsert(int ind , int e);
+		void ListPush(int e);
+		int ListPop();
+		void ListUnshift(int e);
+		int ListShift();
+		void ListDelete(int ind );
+		void ListTraverse(bool (*visit)(int*),bool pre_order=true);
+		void ListUnion(MyLinkList&,MyLinkList& Lunion); // how to use the reference of a class
+		void ListSwap(int ind1,int ind2);
+		void ListSort();  // bubble sort
+		void ListReverse();  
+		void HeapRealloc();
+
+	private:
+		Node* HeadPtr;
+		string m_name;
+	
+};
 //LNode * head;
 #endif //M_2_3_LINK_LIST_H
